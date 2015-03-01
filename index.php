a18531c2763f56520d238f1fe86a0a00b41e98fb
Amr Mekkawy <amr.mekkawy@gmail.com>
2015-03-01 02:51:04 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Background Patterns</title>
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap-3.3.2.css">
+
+	<!-- [Optional] Bootstrap-theme CSS -->
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css"> -->
+
+	<!-- [Optional] Font Awesome (font icons) - http://goo.gl/XfBd3 -->
+	<!-- <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'> -->
+
+	<!-- x-framework -->
+	<link rel='stylesheet' href='assets/css/x-framework-0.1.0.css'>
+
+	<!-- Custom style for this website -->
+	<link rel='stylesheet' href='assets/css/custom-style.css'>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="assets/js/jquery-1.11.2.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="assets/js/bootstrap-3.3.2.min.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!-- .navbar -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<!-- .container -->
+		<div class="container">
+			<!-- .navbar-header -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Background Patterns</a>
+			</div>
+			<!-- /.navbar-header -->
+			
+			<!-- .nav-collapse -->
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</div>
+			<!-- /.nav-collapse -->
+		</div>
+		<!-- /.container -->
+	</nav>
+	<!-- /.navbar -->
+
+	<!-- .container -->
+	<div class="container">
+		<div class="row x-mt-100">
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0001.gif');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0002.jpg');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0003.gif');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0004.jpg');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0005.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0006.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0007.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0008.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0009.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0010.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0011.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0012.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0013.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0014.jpg');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0015.jpg');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0016.gif');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0017.gif');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0018.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0019.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0020.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0021.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0022.png');"></div></div>
+			<div class="col-md-6"><div class="pattern thumbnail" style="background-image: url('assets/img/patterns/0023.jpg');"></div></div>
+		</div>
+	</div>
+	<!-- /.container -->
+
+</body>
+</html>
